e91dbabc133ebf3a39ce025eae5e6800673eeafb
JoaquinTealdi <joaquintealdi25@gmail.com>
2021-09-14 12:22:28 -0300

    subiendo desafio 4 declarando variables

---
+++ src/Desafio4_declarando_variables.java
@@ -0,0 +1,10 @@
+public class Desafio4_declarando_variables {
+    public static void main(String[] args) {
+        String nombre = "Joaquin";
+        int edad = 21;
+        float salario = 70500.00F;
+        boolean carnet = true;
+
+
+    }
+}
