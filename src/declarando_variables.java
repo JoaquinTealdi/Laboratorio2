9d3da15a99ea8b01feb784c53ed073ab2818e1ee
JoaquinTealdi <joaquintealdi25@gmail.com>
2021-09-15 00:43:30 -0300

    subiendo print resis

---
+++ src/declarando_variables.java
@@ -0,0 +1,11 @@
+public class declarando_variables {
+    public static void main(String[] args) {
+        String nombre = "Joaquin";
+        int edad = 21;
+        float salario = 70500.00F;
+        boolean carnet = true;
+
+
+    }
+}
+
---
+++ src/hello_resis.java
@@ -0,0 +1,5 @@
+public class hello_resis {
+    public static void main(String[] args) {
+        System.out.println("hola gente de Resistencia!!!!");
+    }
+}
